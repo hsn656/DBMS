270ca8c7eee7f5c7dfbd4526c3064b048f68246b
Hassan Mohamed <hassan.mhmd656@gmail.com>
2021-12-25 01:25:17 +0200

    insert record V 2.0

--- DBScripts/test.sh
+++ DBScripts/test.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 ##for testing and trying
+
+if `cut -f1 -d: ./databases/hsn/test | grep 1 >> /dev/null 2>/dev/null`
+then
+    echo exits
+else
+    echo not
+fi
--- TableScripts/insertRecord.sh
+++ TableScripts/insertRecord.sh
@@ -9,6 +9,20 @@ function insertField {
       fi
 }
 
+function checkInt {
+    expr $1 + 1 2> /dev/null >> /dev/null
+}
+
+function checkPK {
+    if `cut -f$1 -d: ./databases/hsn/test | grep $2 >> /dev/null 2>/dev/null`
+        then
+        return 1
+    else
+        return 0
+    fi
+}
+
+
 echo "avilable tables are : "
 ls ./databases/$dbname
 read -p "please enter table name : " tableName
@@ -18,22 +32,61 @@ if [ -a ./databases/$dbname/$tableName ]
 
     for (( i=1; i <= $coloumnsNomber; i++ ))
     do
+        ##############################
+        ## inserting primary key field
         if testPK=`grep "%:" ./databases/$dbname/$tableName | cut -d ":" -f$i | grep "%PK%" ` 
             then 
             fieldName=`grep "%:" ./databases/$dbname/$tableName | cut -d ":" -f$i | cut -d "%" -f3 `
+            fieldType=`grep "%:" ./databases/$dbname/$tableName | cut -d ":" -f$i | cut -d "%" -f4 `
             echo "this is primary key it must be uniqe"
-            read -p "enter value of $fieldName : " value
+            read -p "enter ($fieldName) of type ($fieldType) : " value
             
-            insertField $value
-            ################################
-            ## dont forget to check PK value
-            ## dont forget to check data type
+            ###########################
+            ## check data type
+            if [ $fieldType == "int" ]
+                then
+                checkInt $value
+                while [ $? != 0 ]
+                do
+                echo "please enter a valid value"
+                read -p "enter ($fieldName) of type ($fieldType) : " value
+                checkInt $value
+                done
+            fi
 
+            ############################
+            ## check PK constraint
+            checkPK $i $value
+            while [ $? != 0 ]
+            do
+                echo "Violation of PK constraint"
+                echo "please enter a valid value"
+                read -p "enter ($fieldName) of type ($fieldType) : " value
+                checkPK $i $value
+            done
+
+            insertField $value
+
+        #########################    
+        ## inserting normal field
         else
             fieldName=`grep "%:" ./databases/$dbname/$tableName | cut -d ":" -f$i | cut -d "%" -f1 `
-            read -p "enter value of $fieldName : " value
-            #################################
-            ## dont forget to check data type
+            fieldType=`grep "%:" ./databases/$dbname/$tableName | cut -d ":" -f$i | cut -d "%" -f2 `
+            read -p "enter ($fieldName) of type ($fieldType) : " value
+
+            ###########################
+            ## check data type
+            if [ $fieldType == "int" ]
+                then
+                checkInt $value
+                while [ $? != 0 ]
+                do
+                    echo "please enter a valid value"
+                    read -p "enter ($fieldName) of type ($fieldType) : " value
+                    checkInt $value
+                done
+            fi
+
             insertField $value
         fi
     done
