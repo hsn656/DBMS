8d62ebb5bf9a23087247277694d6454aa2650fbe
Hassan Mohamed <hassan.mhmd656@gmail.com>
2022-01-04 21:14:52 +0200

    merge

--- DBScripts/test.sh
+++ DBScripts/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ##for testing and trying
-## %id%int%Pk%
 
-
-grep "%," ./databases/hsn/t | cut -d "," -f$i | cut -d "%" -f2
-grep "%," ./databases/hsn/t | cut -d "," -f$i | cut -d "%" -f3
-
-
-awk -F, 'NR>1 {for(i=1 ;i<=NF ;i++ ) {if (i==NF) print $i; else printf "%s","\t"$i"\t"}}' ./databases/hsn/t
+read x
+if [ -z $x ]
+then
+    echo yes
+else
+    echo no
+fi
